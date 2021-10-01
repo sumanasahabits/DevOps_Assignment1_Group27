9db1d0b1a2ab972c9c0977b363f4af4dd80f61e0
2021mt93340 <2021mt93340@wilp.bits-pilani.ac.in>
2021-10-01 21:17:14 +0530

    calculator addition function

---
+++ app.js
@@ -0,0 +1,16 @@
+var prompt = require("prompt");
+prompt.start();
+console.log("Enter operator (+,-,*,/)");
+prompt.get(["operator"],function(err,res1){
+let op=res1.operator;
+console.log("enter no 1 and no 2");
+prompt.get(["num1","num2"],function(err,res2){
+let no1 = parseFloat(res2.num1);
+let no2 = parseFloat(res2.num2);
+let result;
+if(op=='+'){
+	result=no1+no2;
+}
+console.log('answer:'+result);
+});
+});
